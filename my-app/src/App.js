465837186c676ebef19cc53ce0ba90c3723a3146
simon <ybi4841@gmail.com>
2018-06-11 14:43:03 +0900

    스크립트 나누기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,126 @@
 import React, { Component } from 'react';
 
-const todos = [
-  {
-  id:1,
-  body : "공부",
-  complete : true,
-},{
-    id: 1,
-    body: "공부",
-    complete: false,
-  }
-]
+let count = 0;
 
 class App extends Component {
+  state = {
+    todos : [
+      {
+        id: count++,
+        body: "공부",
+        complete: true
+      },
+      {
+        id: count++,
+        body: "공부",
+        complete: false
+      }
+    ],
+    newTodoBody : ''
+
+  }
+
+
+
+  handleInputChange = e => {
+    this.setState({newTodoBody : e.target.value})
+  }
+
+  handelBtnClick = e => {
+  if (this.state.newTodoBody) {
+      const newTodo = {
+        body : this.state.newTodoBody,
+        complete :false,
+        id: count++
+      };
+      this.setState({
+        todos : [
+          ...this.state.todos,
+          newTodo
+        ],
+        newTodoBody:''
+      });
+   }
+  }
+
+handletodoItemComplete = id =>{
+  this.setState({
+      todos :  this.state.todos.map(t => {
+            const newtodo = {
+              ...t
+            }
+            if (t.id === id) {
+              newtodo.complete = true;
+            }
+            return newtodo
+          })
+        })
+}
+
+handleDel = id => {
+  this.setState({
+    todos: this.state.todos.filter(t => t.id !== id)
+
+        })
+
+
+}
+
+
+  
   render() {
+  const {todos, newTodoBody} = this.state
+
     return (
       <div>
         <h1>할일 목록</h1>
+        <label>
+        새 할일
+        <input type="text" value={newTodoBody} onChange={this.handleInputChange} />
+      <button onClick={this.handelBtnClick}>추가</button>
+        </label>
       <ul>
         {
-          todos.map(todos => (
-            <li key={todo.id}>{todo.body}</li>
-          ))
+          todos.map(todo => ( <TodoItem 
+            key = {todo.id} 
+           {...todo}
+             onComplete = {this.handletodoItemComplete} 
+             del = {this.handleDel} />
+            
+            ))
 
         }
+
       </ul>
-
       </div>
-
     );
   }
 }
+
+
+
+class TodoItem extends Component {
+render(){
+
+  const {id,body,complete, onComplete, del} =this.props
+
+return (
+<li className = { complete ? 'complete' : '' }> 
+
+{ body }
+
+    < button onClick = {e => {onComplete(id) }} > 완료</button >
+
+      <button onClick={ e => {del(id)}}> 삭제</button>
+           
+           
+            </li >
+
+  )
+
+}
+
+}
+
 
 export default App;
